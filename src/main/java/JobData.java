04c7005765a2631adfba168d8360eee1f008615c
AndyFitz <fitzandy4@gmail.com>
2022-02-03 17:29:35 -0500

    Updates files to pass

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -74,12 +74,13 @@ public class JobData {
         loadData();
 
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        value = value.toLowerCase();
 
         for (HashMap<String, String> row : allJobs) {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value)) {
                 jobs.add(row);
             }
         }
@@ -98,18 +99,24 @@ public class JobData {
         // load data, if not already loaded
         loadData();
 
-        // TODO - implement this method
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        value = value.toLowerCase();
 
         for (HashMap<String, String> row : allJobs) {
-            for (String jobResult : row.values()) {
-                if (jobResult.toLowerCase().contains(value.toLowerCase())) {
+            for (String key : row.keySet()) {
+                if (row.get(key).toLowerCase().contains(value)) {
                     jobs.add(row);
+                    break;
                 }
             }
         }
+
         return jobs;
     }
+
+    // TODO - implement this method
+    // return null;
+//}
 
     /**
      * Read in data from a CSV file and store it in a list
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -70,7 +70,7 @@ public class TechJobs {
         }
     }
 
-    // ﻿Returns the key of the selected item from the choices Dictionary
+    // Returns the key of the selected item from the choices Dictionary
     private static String getUserSelection(String menuHeader, HashMap<String, String> choices) {
 
         int choiceIdx = -1;
@@ -112,24 +112,26 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while (!validChoice);
+        } while(!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        if (someJobs.size() < 1) {
-            System.out.println("No Results");
-        } else {
-            for (HashMap<String, String> job : someJobs) {
-                System.out.println("");
-                System.out.println("*****");
-                for (Map.Entry<String, String> jobResult : job.entrySet()) {
-                    System.out.println(jobResult.getKey() + ": " + jobResult.getValue());
+
+        if (someJobs.size() > 0) {
+            for (HashMap<String, String> aJob : someJobs) {
+                System.out.println("\n*****");
+                for(Map.Entry<String, String> data: aJob.entrySet()) {
+                    System.out.println(data.getKey() + ": " + data.getValue());
                 }
                 System.out.println("*****");
             }
+        }
+
+        else {
+            System.out.print("No Results");
         }
     }
 }
